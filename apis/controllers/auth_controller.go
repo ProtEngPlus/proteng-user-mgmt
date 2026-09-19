9dab65103ac4e797ab2d719621681346a38e9ae2
BadLuckZ <thanagorn8802@gmail.com>
2026-09-19 22:28:12 +0700

    fix: remove verification token after use

--- apis/controllers/auth_controller.go
+++ apis/controllers/auth_controller.go
@@ -13,6 +13,7 @@ import (
 	"github.com/thanhpk/randstr"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/protengplus/proteng-user-mgmt/configs"
 	"github.com/protengplus/proteng-user-mgmt/database"
@@ -393,31 +394,37 @@ func (ac *AuthController) VerifyEmail(c *gin.Context) {
 	verificationToken := c.Params.ByName("verificationToken")
 	emailVerificationToken := utils.Encode(verificationToken)
 
-	// Update User in Database
-	query := bson.D{{Key: "emailVerificationToken", Value: emailVerificationToken}, {Key: "emailVerificationTokenExpire", Value: bson.D{{Key: "$gt", Value: time.Now()}}}}
-	update := bson.D{{Key: "$set", Value: bson.D{{Key: "is_verified", Value: true}}}}
-	result, err := ac.collection.UpdateOne(context.Background(), query, update)
-
-	if result.MatchedCount == 0 {
-		apiutil.ApiResponseErrorBadRequest(c, fmt.Errorf("invalid token"), "Token is invalid or has expired")
-		return
+	// Verify token in one atomic operation
+	query := bson.D{
+		{Key: "emailVerificationToken", Value: emailVerificationToken},
+		{Key: "emailVerificationTokenExpire", Value: bson.D{{Key: "$gt", Value: time.Now()}}},
+	}
+	update := bson.D{
+		{Key: "$set", Value: bson.D{{Key: "is_verified", Value: true}}},
+		{Key: "$unset", Value: bson.D{
+			{Key: "emailVerificationToken", Value: ""},
+			{Key: "emailVerificationTokenExpire", Value: ""},
+		}},
 	}
 
-	if err != nil {
-		apiutil.ApiResponseForbidden(c, err)
-		return
-	}
-
-	// Find user
 	var user models.User
-	err = ac.collection.FindOne(context.Background(), query).Decode(&user)
+	err := ac.collection.FindOneAndUpdate(
+		context.Background(), query, update,
+		options.FindOneAndUpdate().SetReturnDocument(options.After),
+	).Decode(&user)
 	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			apiutil.ApiResponseErrorBadRequest(c, fmt.Errorf("invalid token"), "Token is invalid or has expired")
+			return
+		}
 		apiutil.ApiResponseForbidden(c, err)
 		return
 	}
 
 	// Generate Tokens
-	duration, err := time.ParseDuration(configs.Config.AccessTokenExpiredIn)
+	var duration time.Duration
+	duration, err = time.ParseDuration(configs.Config.AccessTokenExpiredIn)
+
 	if err != nil {
 		apiutil.ApiResponseInternalServerError(c, err)
 		return // Return an error if parsing fails
